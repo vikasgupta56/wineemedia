ea78497627f9acb4fc30ef61244a1335a8ab1f55
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-01-27 18:51:41 +0530

    about text

--- src/components/about/Section1.jsx
+++ src/components/about/Section1.jsx
@@ -3,11 +3,11 @@ import Navbar from '../navbar/Navbar'
 
 const Section1 = () => {
   return (
-    <div className='w-full h-screen text-white font-[font6] flex flex-col items-center justify-center '>
+    <div className='w-full h-[100svh] text-white font-[font6] flex flex-col items-end justify-end pb-[6vw] px-[2vw]'>
         <Navbar/>
-        <h1 className='text-[5vw] leading-none uppercase'>Discover our studio</h1>
-        <h1 className='text-[5vw] uppercase'>and vision</h1>
-        <h6 className='w-[80%] mg:w-[40%]   text-[3vw] md:text-[1.1vw] mt-[1vw] text-center'>We are a design-focused studio passionate about using our creative skills to develop brand experiences. We aim to transform ideas into visual stories that bring brands to life through print and digital.</h6>
+        {/* <h1 className='text-[5vw] leading-none uppercase'>Discover our studio</h1> */}
+        {/* <h1 className='text-[5vw] uppercase'>and vision</h1> */}
+        <h6 className='w-[80%] md:w-[50%]  text-[3vw] md:text-[2.1vw]  text-left'>We are a design-focused agency passionate about using our creative skills to develop brand experiences. We aim to transform ideas into visual stories that bring brands to life through print and digital.</h6>
     </div>
   )
 }
