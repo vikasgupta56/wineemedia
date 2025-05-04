e18491b3b070902c82fb625845d9a7b2668c9c33
ayush-zerror <ayush.zerror@gmail.com>
2025-05-04 17:08:49 +0530

    fixed path

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,15 @@
-import SmoothScroller from "@/components/smoothScroll/SmoothScroll";
 import Transition from "@/components/transition/Transition";
 import "@/styles/globals.css";
 import 'remixicon/fonts/remixicon.css'
 import { ToastContainer, toast } from 'react-toastify';
+import SmoothScroller from "@/components/smoothScroll/SmoothScroller";
 
 
 export default function App({ Component, pageProps, router }) {
   return (
     <>
       <Transition>
-        <SmoothScroller />
+        <SmoothScroller/>
         <Component key={router.route} {...pageProps} />
       </Transition>
       <ToastContainer 
